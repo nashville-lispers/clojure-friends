40c36eef86dba3478d883874a3686de7ae962084
Nate West <natescott.west@gmail.com>
2015-03-19 18:56:17 -0500

    init niftyn8's implementation as an example

---
+++ niftyn8/clojure-friends/test/clojure_friends/test/handler.clj
@@ -0,0 +1,13 @@
+(ns clojure-friends.test.handler
+  (:use clojure.test
+        ring.mock.request
+        clojure-friends.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= 404 (:status response))))))
